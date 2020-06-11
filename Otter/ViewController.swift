8119259b0d2e67193c29b6361beab7365a405851
Amy Huang <ach243@cornell.edu>
2020-06-11 16:16:28 -0500

    Initial Commit

---
+++ Otter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Otter
+//
+//  Created by Amy Chin Siu Huang on 6/11/20.
+//  Copyright © 2020 Amy Chin Siu Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
